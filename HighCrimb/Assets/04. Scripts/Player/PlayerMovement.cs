ab6cea7f19cb024d7de7770f3ea78c3123d6c517
DESKTOP-GVL0O4M\hwaya <hwayanggames@gmail.com>
2024-04-11 12:19:12 +0900

    [Update] 이동 공식에 가/감속 추가

--- HighCrimb/Assets/04. Scripts/Player/PlayerMovement.cs
+++ HighCrimb/Assets/04. Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,14 @@ public class PlayerMovement : MonoBehaviour
     [Range(300, 400)]
     public float moveSpeed = 300f;
 
+    [SerializeField]
+    [Range(0, 100)]
+    public float accelSpeed = 1f;
+
+    [SerializeField]
+    [Range(0, 100)]
+    public float decelSpeed = 1f;
+
     private Rigidbody2D playerRb;
 
     private void Awake()
@@ -17,6 +25,24 @@ public class PlayerMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
-        playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeed * Time.deltaTime;
+        float moveControlValue = MoveControl();
+
+        playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * (moveSpeed + moveControlValue) * Time.deltaTime;
+        Debug.Log("MoveSpeed" + playerRb.velocity);
+    }
+
+    //Accel과 Decel은 다른 상호작용으로 일어날 수 있는 요소이기 때문에 public으로 타 객체에서 호출 할 수 있게 설정
+    public float MoveControl()
+    {
+        if(Input.GetAxisRaw("Accel") == 1)
+        {
+            return accelSpeed;
+        }
+        else if(Input.GetAxisRaw("Decel") == 1)
+        {
+            return -decelSpeed;
+        }
+
+        return 0f;
     }
 }
